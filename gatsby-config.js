9b45d2a9b6f2485e5686767f1309f97c2224cf2e
Justin N <jstnnixon@gmail.com>
2020-09-10 19:22:33 +0200

    transform .md function success

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,36 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: 'Gatsby Proof of Concept',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false
+            },
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/docs`,
+        name: "docs",
+      },
+    },
+  ],
+};
---
+++ src/docs/about.md
@@ -0,0 +1,12 @@
+---
+path: "/about"
+Title: About Page
+Date: 10.09.2020
+---
+
+
+#This is the about page
+
+
+
+test text here
