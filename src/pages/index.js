93a8c38a425ee80248273102112c85f01bb7e619
Justin N <jstnnixon@gmail.com>
2020-09-10 18:04:01 +0200

    added picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Justin!</div>
+  return <div style={{ color: `purple`}}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+        </div>
 }
